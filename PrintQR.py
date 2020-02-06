2c6f20a0a751132885817ee2505b9ddbc5b0e811
rankinjonathan <43206474+rankinjonathan@users.noreply.github.com>
2020-02-06 14:37:25 +0000

    Add files via upload

---
+++ PrintQR.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+# Main script for Adafruit Internet of Things Printer 2.  Monitors button
+# for taps and holds, performs periodic actions (Twitter polling by default)
+# and daily actions (Sudoku and weather by default).
+# Written by Adafruit Industries.  MIT license.
+#
+# MUST BE RUN AS ROOT (due to GPIO access)
+#
+# Required software includes Adafruit_Thermal, Python Imaging and PySerial
+# libraries. Other libraries used are part of stock Python install.
+#
+# Resources:
+# http://www.adafruit.com/products/597 Mini Thermal Receipt Printer
+# http://www.adafruit.com/products/600 Printer starter pack
+
+from __future__ import print_function
+import RPi.GPIO as GPIO
+import subprocess, time, socket
+from PIL import Image
+from Adafruit_Thermal import *
+
+ledPin       = 18
+buttonPin    = 23
+holdTime     = 2     # Duration for button hold (shutdown)
+tapTime      = 0.01  # Debounce time for button taps
+nextInterval = 0.0   # Time of next recurring operation
+dailyFlag    = False # Set after daily trigger occurs
+lastId       = '1'   # State information passed to/from interval script
+printer      = Adafruit_Thermal("/dev/serial0", 19200, timeout=5)
+
+
+
+# Initialization
+
+# Use Broadcom pin numbers (not Raspberry Pi pin numbers) for GPIO
+GPIO.setmode(GPIO.BCM)
+
+# Enable LED and button (w/pull-up on latter)
+GPIO.setup(ledPin, GPIO.OUT)
+GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+# LED on while working
+GPIO.output(ledPin, GPIO.HIGH)
+
+# Processor load is heavy at startup; wait a moment to avoid
+# stalling during greeting.
+time.sleep(30)
+
+# Print greeting image
+printer.printImage(Image.open('gfx/myqrcode.png'), True)
+printer.feed(3)
+GPIO.output(ledPin, GPIO.LOW)
+
